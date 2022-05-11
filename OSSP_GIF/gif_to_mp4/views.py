104d78b7d38ab3ab2de6562f92bb6c45bf65d1ca
sangyoon kim <sangyun0904@gmail.com>
2022-05-11 13:34:40 +0900

    Add calculate start and end time in view.py and import youtube-dl

--- OSSP_GIF/gif_to_mp4/views.py
+++ OSSP_GIF/gif_to_mp4/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import URLform
 from django.views import View
+import youtube_dl
 # Create your views here.
 class MainView(View): 
     template_name = 'gif_to_mp4/main.html'
@@ -15,5 +16,17 @@ class MainView(View):
         print(form['youtube_link'].value())
         #print(form['your_name'])
         #ctx = {'form':form}
-        ctx = {'form':form}
+
+        start = 0
+        end = 0
+        if form.is_valid():
+            url = form.cleaned_data.get("youtube_link")
+            start = form.cleaned_data.get("start_minute")*60 + form.cleaned_data.get("start_second")
+            end = form.cleaned_data.get("end_minute")*60 + form.cleaned_data.get("end_second")
+        print(start, end)
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        ctx = {'form':form, 'start':start, 'end':end}
         return render(request, self.template_name,ctx)
