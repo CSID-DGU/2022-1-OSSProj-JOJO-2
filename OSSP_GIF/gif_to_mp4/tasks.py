9ae01aa09e750f4113b8de08a416575b7a3d14c6
JongRokBaek <whiteleo96@gmail.com>
2022-06-01 19:50:33 +0900

    Add asynchronou processing

---
+++ OSSP_GIF/gif_to_mp4/tasks.py
@@ -0,0 +1,27 @@
+import youtube_dl,os, json
+from celery import shared_task
+
+from django.shortcuts import redirect
+@shared_task() 
+def downloand_video(data):
+    data = json.loads(data)
+    url, ss ,to = data['url'], data['ss'],data['to']
+
+    if os.path.exists('video.mp4'):
+        os.remove('video.mp4')
+
+    if os.path.exists('video.gif'):
+        os.remove('video.gif')
+
+    ydl_opts = {
+        'format': "best",
+        'videoformat' : "mp4",
+        'outtmpl' : "video.mp4",
+        'external_downloader': 'ffmpeg',
+        'external_downloader_args':  ["-ss", ss, "-to", to],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
--- OSSP_GIF/gif_to_mp4/views.py
+++ OSSP_GIF/gif_to_mp4/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from asyncio import tasks
+from django.shortcuts import render,redirect
 from .forms import URLform
 from django.views import View
 from django.contrib import messages
-
-import os
-import youtube_dl
-from moviepy.editor import *
-
-from django.http import FileResponse, response
+import json
+from .tasks import downloand_video
+from django.http import FileResponse
 from django.core.files.storage import FileSystemStorage
+from moviepy.editor import *
+import time
+import os
 
 # Create your views here.
 class MainView(View): 
@@ -17,6 +18,36 @@ class MainView(View):
     def get(self, request):
         form = URLform()
         ctx = {'form':form}
+        try:
+            if task_id:
+                print("task_id 있음 으로 들어옴")
+                print(task_id)
+                print(task_id.ready())
+                while not task_id.ready():
+                    time.sleep(2)
+                    print("while문")
+
+                if task_id.failed():
+                    messages.add_message(request,30, '오류가 발생했습니다 다시 시도해 주세요')
+                    return render(request, self.template_name, ctx)
+                messages.add_message(request,30, '동영상이 모두 다운로드 되었습니다!')
+                title = "video"
+                clip = VideoFileClip(title + '.mp4')
+                clip.write_gif(title + '.gif')
+                clip.close()
+                os.remove('video.mp4')
+                file_path = os.path.abspath("./")
+                file_name = os.path.basename("./" + title + ".gif")
+                fs = FileSystemStorage(file_path)
+                response = FileResponse(fs.open(file_name, 'rb'),
+                                        content_type='image/gif')
+                response['Content-Disposition'] = f'attachment; filename=video.gif'
+                # 여기서 return 하면 새로고침 할 때 계속 post라서 문제임.. 고쳐야함
+                return response
+        except:
+            print("task 로 안 들어옴")
+            #print(task_id.ready())
+            pass
         return render(request, self.template_name, ctx)
 
     def post(self, request):
@@ -29,54 +60,21 @@ class MainView(View):
             end_min = form.cleaned_data.get("end_minute")
             end_sec = form.cleaned_data.get("end_second")
             resolution = form.cleaned_data.get("resolution")
-            diff = 60 * start_min + start_sec - (60 * end_min - end_sec)
+            diff =  60 * end_min + end_sec - (60 * start_min + start_sec)
             if diff <0 or 5 < diff:
                 messages.add_message(request, 30, '시작 시간과 끝 시간의 차이를 1~5초 사이로 맞춰주세요')
+                print(request)
                 return render(request, self.template_name, ctx) 
         else:
             return render(request, self.template_name,ctx)
-
         ss = f"00:{start_min:02}:{start_sec:02}.00"
         to = f"00:{end_min:02}:{end_sec:02}.00"
-
-        try:
-            fs.close()
-        except:
-            print()
-
-        if os.path.exists('video.mp4'):
-            os.remove('video.mp4')
-
-        if os.path.exists('video.gif'):
-            os.remove('video.gif')
-
-        title = "video"
-
-        ydl_opts = {
-            'format': "best",
-            'videoformat' : "mp4",
-            'outtmpl' : "video.mp4",
-            'external_downloader': 'ffmpeg',
-            'external_downloader_args':  ["-ss", ss, "-to", to]
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            #title = ydl.extract_info(url, download=False)['title']
-
-        clip = VideoFileClip(title + '.mp4')
-        clip.write_gif(title + '.gif')
-        clip.close()
-        
-        os.remove('video.mp4')
-
-        file_path = os.path.abspath("./")
-        file_name = os.path.basename("./" + title + ".gif")
-        fs = FileSystemStorage(file_path)
-        response = FileResponse(fs.open(file_name, 'rb'),
-                                content_type='image/gif')
-        response['Content-Disposition'] = f'attachment; filename=video.gif'
-
-        return response
-    
-
+        dic = {'url' : url, "ss" : ss, "to" : to}
+        data = json.dumps(dic, indent = 4)
+        global task_id 
+        t = downloand_video.delay(data)
+        task_id = t
+        print(t.ready())
+        print(task_id.ready())
+        messages.add_message(request, 30, '유투브를 다운로드 중 입니다 <br/> 창을 닫거나 새로고침하지 말아주세요')
+        return redirect('gif_to_mp4:home') 
