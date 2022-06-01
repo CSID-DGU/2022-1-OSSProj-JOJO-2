03c11f56cfe42d8b6e421bb9bc7fbde7e4b16f10
JongRokBaek <whiteleo96@gmail.com>
2022-06-01 23:33:36 +0900

    update celery_task_falure_condition

--- OSSP_GIF/gif_to_mp4/tasks.py
+++ OSSP_GIF/gif_to_mp4/tasks.py
@@ -1,27 +1,30 @@
 import youtube_dl,os, json
 from celery import shared_task
-
-from django.shortcuts import redirect
+from celery.exceptions import Ignore
 @shared_task() 
 def downloand_video(data):
-    data = json.loads(data)
-    url, ss ,to = data['url'], data['ss'],data['to']
+    try:
+        data = json.loads(data)
+        url, ss ,to = data['url'], data['ss'],data['to']
 
-    if os.path.exists('video.mp4'):
-        os.remove('video.mp4')
+        if os.path.exists('video.mp4'):
+            os.remove('video.mp4')
 
-    if os.path.exists('video.gif'):
-        os.remove('video.gif')
+        if os.path.exists('video.gif'):
+            os.remove('video.gif')
 
-    ydl_opts = {
-        'format': "best",
-        'videoformat' : "mp4",
-        'outtmpl' : "video.mp4",
-        'external_downloader': 'ffmpeg',
-        'external_downloader_args':  ["-ss", ss, "-to", to],
-    }
+        ydl_opts = {
+            'format': "best",
+            'videoformat' : "mp4",
+            'outtmpl' : "video.mp4",
+            'external_downloader': 'ffmpeg',
+            'external_downloader_args':  ["-ss", ss, "-to", to],
+        }
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except:
+        raise Ignore()
+        
 
 
