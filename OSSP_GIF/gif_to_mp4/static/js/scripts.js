5ecc5a24a06b5e422184d18474fecfb41bb818f6
JongRokBaek <whiteleo96@gmail.com>
2022-06-08 20:21:11 +0900

    fix javascript error

--- OSSP_GIF/gif_to_mp4/static/js/scripts.js
+++ OSSP_GIF/gif_to_mp4/static/js/scripts.js
@@ -7,7 +7,6 @@
 // Use this file to add JavaScript to your project
 
 // 로딩 화면 구현할 경우 수정
-var downloading = false;
 const GifSocket = new WebSocket(
   'ws://'
   + window.location.host
@@ -17,7 +16,6 @@ GifSocket.onmessage = function(e) {
   if (data.message == 'Done'){
     window.location = 'gif';
     document.getElementById("submitButton").disabled = false;
-    downloading = true
     document.getElementById("message").textContent = "동영상이 모두 다운로드 되었습니다!";
     spinner.style.visibility = 'visible';
   }else{
@@ -28,7 +26,6 @@ GifSocket.onmessage = function(e) {
 };
 
 function clickSubmit(this1){
-  document.getElementById("submitButton").disabled = true;
   var url = document.querySelector('#youtube_link').value;
   var s_m = document.querySelector('#start_minute').value;
   var s_s = document.querySelector('#start_second').value;
@@ -44,6 +41,7 @@ function clickSubmit(this1){
     document.getElementById("message").textContent = "1초 이상의 값을 입력해 주세요.";
   }else if (diff < 6 && 0 < diff){
     spinner.style.visibility = 'visible';
+    document.getElementById("submitButton").disabled = true;
     console.log("form 보냄 socekt도 보냄")
     GifSocket.send(data = JSON.stringify({
       'status' : '',
@@ -54,7 +52,6 @@ function clickSubmit(this1){
       'end_second' : e_s
     }))
     // this1.form.submit(); 그냥 socket으로 처리
-    downloading = true;
     document.getElementById("message").textContent = "동영상을 다운로드 중 입니다.  새로고침을 하지 말아주세요";
     this1.form.reset();
   }
